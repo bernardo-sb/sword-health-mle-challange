e8b04b135725af4d0c3425db03916916d7c32055
bernardo-sb <bernardo@shippedbrain.com>
2025-03-21 23:57:09 +0000

    create runner

--- message/main.py
+++ message/main.py
@@ -1,9 +1,18 @@
+import os
 import typer
 from message.data import transform_features_py  # noqa
 from message.data import transform_features_sql  # noqa
 from message.data import get_features
-from prompts.prompt import SYSTEM, SYSTEM_FEEDBACK, EXTRA_FEEDBACK, TONE_PROMPT, GENERIC_PROMPT, ENGAGEMENT_PROMPT, FACTUALITY_PROMPT, OTHER_PROMPT
+from message.config import get_settings
+from message.model import ChatModel
+from message.prompts import SYSTEM_BASE, SYSTEM_FEEDBACK, EXTRA_FEEDBACK, TONE_PROMPT, GENERIC_PROMPT, ENGAGEMENT_PROMPT, FACTUALITY_PROMPT, OTHER_PROMPT
 from enum import StrEnum
+from uuid import uuid4
+import json
+import asyncio
+
+settings = get_settings()
+chat_model = ChatModel()
 
 class FeedbackOption(StrEnum):
     TONE = "tone"
@@ -33,43 +42,108 @@ def transform():
 
     return
 
+def load_chat_history(chat_id: str) -> list[dict[str, str]]:
+    """Load chat history from JSONL file."""
+    os.makedirs(".chats", exist_ok=True)
+    chat_file = os.path.join(".chats", f"{chat_id}.jsonl")
 
-@app.command()
-def get_message(
-    session_group: str,
-    chat_history: list[dict[str, str]] | None = None,
-    session_group_limit: int | None = None,
-    feedback_cat: FeedbackOption = FeedbackOption.OTHER,
-    extra_feedback: str = None ) -> str:
+    if not os.path.exists(chat_file):
+        with open(chat_file, "w") as f:
+            return []
+
+    with open(chat_file, "r") as f:
+        return [json.loads(line) for line in f]
+
+def prompt_for_acceptance() -> str:
+    """Prompt user for message acceptance."""
+    response = None
+    while not response:
+        response = input("Accept | Edit | Reject: ").strip().lower()
+        if response not in ["accept", "edit", "reject"]:
+            print("Invalid response. Please enter 'accept', 'edit', or 'reject'.")
+            response = None
+
+    return response
     
-    # Perform checks
-    if extra_feedback and not chat_history:
-        raise ValueError("'extra_feedback' can only be provided when 'chat_history' is provided")
-    if session_group_limit and session_group_limit > 0:
-        raise ValueError("'session_group_limit' must be greater than 0")
-    FeedbackOption(feedback_cat) # raises ValueError if not valid option
-
-    extra_feedback = extra_feedback if extra_feedback else ""   
-
-    session_group_limit = session_group if not session_group_limit else session_group_limit[:session_group_limit]
+def prompt_for_feedback() -> tuple[str, str]:
+    """Prompt user for feedback."""
+    category = None
+    while not category:
+        category = input("Editing Reasons (optional) - [tone, generic, engagement, factuality, other]: ").lower().strip()
+        try:
+            category = "other" if not category else category
+            category = FeedbackOption(category)
+        except ValueError:
+            print("Invalid category. Please enter 'tone', 'generic', 'engagement', 'factuality', or 'other'.")
     
-    features = get_features(session_group=session_group_limit)  # noqa
+    feedback = None
+    while not feedback:
+        feedback = input("Feedback: ").strip().lower()
+        if not feedback:
+            print("Feedback cannot be empty.")
+    return category, feedback
 
-    messages = [{"role": "system", "content": SYSTEM.format(session_data=features)}]
-
-    if chat_history:
-        messages.extend(chat_history)
-    
-    if extra_feedback:
-        messages.append({"role": "system", "content": SYSTEM_FEEDBACK})
-        messages.append({"role": "system", "content": FEEDBACK_PROMPT_MAP[feedback_cat]})
-        messages.append({"role": "user", "content": EXTRA_FEEDBACK.format(extra_feedback=extra_feedback)})
-
-    print("Sending:", messages)
-    message = chat_model.get_completion(
+def llm(messages: list[dict[str, str]]) -> str:
+    return chat_model.get_completion(
             temperature=0,
             model="gpt-4o-mini",
             messages=messages
     )
 
-    return message
+async def save_chat_history(chat_id: str, chat_history: list[dict[str, str]]):
+    chat_file = os.path.join(".chats", f"{chat_id}.jsonl")
+    with open(chat_file, "w") as f:
+        for message in chat_history:
+            f.write(json.dumps(message) + "\n")
+
+async def _run(session_group: str) -> str:
+    try:
+        print("Loading session group:", session_group)
+
+        features = get_features(session_group=session_group)  # noqa
+
+        chat_id = str(uuid4())
+        print("Starting chat with id:", chat_id)
+
+        chat_history = load_chat_history(chat_id)
+
+        if not chat_history:
+            chat_history.append({"role": "system", "content": SYSTEM_BASE.format(session_data=features)})
+
+        print("="*50)
+        while True:
+            message = llm(chat_history)
+            
+            print("Message:", message)
+
+            acceptance = prompt_for_acceptance()
+
+            if acceptance == "accept":
+                chat_history.append({"role": "assistant", "content": message})
+                _ = asyncio.create_task(save_chat_history(chat_id, chat_history))
+            if acceptance == "edit":
+                category, feedback = prompt_for_feedback()
+                
+                # TODO: only save accepted messages (e.g. use tem_chat_history)
+                chat_history.append({"role": "system", "content": SYSTEM_FEEDBACK.format(feedback_prompt=FEEDBACK_PROMPT_MAP[FeedbackOption(category)])})
+                chat_history.append({"role": "user", "content": EXTRA_FEEDBACK.format(extra_feedback=feedback)})
+
+                message = llm(chat_history)
+
+                print("Message:", message)
+                print("-"*50)
+            if acceptance == "reject":
+                message = input("Write your answer: ")
+                chat_history.append({"role": "assistant", "content": message})
+                asyncio.create_task(save_chat_history(chat_id, chat_history))
+    except:
+        print()
+        print("="*50)
+        print("\nShutting down...")
+        await save_chat_history(chat_id, chat_history)
+        
+        raise typer.Exit()
+
+@app.command()
+def get_message(session_group: str) -> str:
+    asyncio.run(_run(session_group))
